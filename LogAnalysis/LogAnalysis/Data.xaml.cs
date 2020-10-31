b4e638bb7e2e763866fb7cd036caaf54963f0efc
Kaitlyn Freeborn <klf160230@utdallas.edu>
2020-10-30 19:30:43 -0500

    pushing updates even though code does not compile

--- LogAnalysis/LogAnalysis/Data.xaml.cs
+++ LogAnalysis/LogAnalysis/Data.xaml.cs
@@ -19,6 +19,11 @@ using Microsoft.Win32;
 using System.Collections;
 using System.Data.OleDb;
 using System.Data;
+using Microsoft.Office.Interop.Excel;
+using System.Runtime.InteropServices;
+using Window = Microsoft.Office.Interop.Excel.Window;
+using DataTable = System.Data.DataTable;
+
 
 namespace LogAnalysis
 {
@@ -29,59 +34,184 @@ namespace LogAnalysis
     {
         public Data()
         {
+            string fname = MyVariables.txtFile;
+
+
+
             // COMMENTED OUT SO THAT CODE WILL COMPILE, WORKING ON THIS SECTION
 
+            String constr = "Provider=Microsoft.Jet.OLEDB.4.0;Data Source=" +
+                       fname +
+                       ";Extended Properties='Excel 8.0;HDR=YES;';";
 
-            //InitializeComponent();
-            //string filePath = string.Empty;
-            //string fileExt = string.Empty;
-            //OpenFileDialog file = new OpenFileDialog(); //open dialog to choose file 
-            //if (file.ShowDialog() = System.Windows.Forms.DialogResult.OK) //if there is a file choosen by the user  
-            //{
-            //    filePath = file.FileName; //get the path of the file  
-            //    fileExt = System.IO.Path.GetExtension(filePath); //get the file extension  
-            //    if (fileExt.CompareTo(".xls") == 0 || fileExt.CompareTo(".xlsx") == 0)
-            //    {
-            //        try
-            //        {
-            //            DataTable dtExcel = new DataTable();
-            //            dtExcel = ReadExcel(filePath, fileExt); //read excel file  
-            //            dataGridView1.Visible = true;
-            //            dataGridView1.DataSource = dtExcel;
-            //        }
-            //        catch (Exception ex)
-            //        {
-            //            MessageBox.Show(ex.Message.ToString());
-            //        }
-            //    }
-            //    else
-            //    {
-            //        MessageBox.Show("Please choose .xls or .xlsx file only.", "Warning", MessageBoxButtons.OK, MessageBoxIcon.Error); //custom messageBox to show error  
-            //    }
+            OleDbConnection con = new OleDbConnection(constr);
+            OleDbCommand oconn = new OleDbCommand("select * from [Sheet1$]", con);
+            con.Open();
 
-            }
+            OleDbDataAdapter sda = new OleDbDataAdapter(oconn);
+            DataTable data = new DataTable();
+            sda.Fill(data);
+            dataGridView1.DataSource = data;
+
+
+
+
         }
 
-        //public DataTable ReadExcel(string fileName, string fileExt)
-        //{
-        //    string conn = string.Empty;
-            
-        //    DataTable dtexcel = new DataTable();
-        //    if (fileExt.CompareTo(".xls") == 0)
-        //        conn = @"provider=Microsoft.Jet.OLEDB.4.0;Data Source=" + fileName + ";Extended Properties='Excel 8.0;HRD=Yes;IMEX=1';"; //for below excel 2007  
-        //    else
-        //        conn = @"Provider=Microsoft.ACE.OLEDB.12.0;Data Source=" + fileName + ";Extended Properties='Excel 12.0;HDR=NO';"; //for above excel 2007  
-        //    using (OleDbConnection con = new OleDbConnection(conn))
-        //    {
-        //        try
-        //        {
-        //            OleDbDataAdapter oleAdpt = new OleDbDataAdapter("select * from [sheet1$]", con); //here we read data from sheet1  
-        //            oleAdpt.Fill(dtexcel); //fill excel data into dataTable  
-        //        }
-        //        catch { }
-        //    }
-        //    return dtexcel;
-        //}
+        dynamic Window.Activate()
+        {
+            throw new NotImplementedException();
+        }
+
+        public dynamic ActivateNext()
+        {
+            throw new NotImplementedException();
+        }
+
+        public dynamic ActivatePrevious()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Close(object SaveChanges, object Filename, object RouteWorkbook)
+        {
+            throw new NotImplementedException();
+        }
+
+        public dynamic LargeScroll(object Down, object Up, object ToRight, object ToLeft)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Window NewWindow()
+        {
+            throw new NotImplementedException();
+        }
+
+        public dynamic _PrintOut(object From, object To, object Copies, object Preview, object ActivePrinter, object PrintToFile, object Collate, object PrToFileName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public dynamic PrintPreview(object EnableChanges)
+        {
+            throw new NotImplementedException();
+        }
+
+        public dynamic ScrollWorkbookTabs(object Sheets, object Position)
+        {
+            throw new NotImplementedException();
+        }
+
+        public dynamic SmallScroll(object Down, object Up, object ToRight, object ToLeft)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int PointsToScreenPixelsX(int Points)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int PointsToScreenPixelsY(int Points)
+        {
+            throw new NotImplementedException();
+        }
+
+        public dynamic RangeFromPoint(int x, int y)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void ScrollIntoView(int Left, int Top, int Width, int Height, object Start)
+        {
+            throw new NotImplementedException();
+        }
+
+        public dynamic PrintOut(object From, object To, object Copies, object Preview, object ActivePrinter, object PrintToFile, object Collate, object PrToFileName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Microsoft.Office.Interop.Excel.Application Application => throw new NotImplementedException();
+
+        public XlCreator Creator => throw new NotImplementedException();
+
+        dynamic Window.Parent => throw new NotImplementedException();
+
+        public Range ActiveCell => throw new NotImplementedException();
+
+        public Chart ActiveChart => throw new NotImplementedException();
+
+        public Pane ActivePane => throw new NotImplementedException();
+
+        public dynamic ActiveSheet => throw new NotImplementedException();
+
+        public dynamic Caption { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public bool DisplayFormulas { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public bool DisplayGridlines { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public bool DisplayHeadings { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public bool DisplayHorizontalScrollBar { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public bool DisplayOutline { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public bool _DisplayRightToLeft { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public bool DisplayVerticalScrollBar { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public bool DisplayWorkbookTabs { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public bool DisplayZeros { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public bool EnableResize { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public bool FreezePanes { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public int GridlineColor { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public XlColorIndex GridlineColorIndex { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+
+        public int Index => throw new NotImplementedException();
+
+        public string OnWindow { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+
+        public Panes Panes => throw new NotImplementedException();
+
+        public Range RangeSelection => throw new NotImplementedException();
+
+        public int ScrollColumn { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public int ScrollRow { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+
+        public Sheets SelectedSheets => throw new NotImplementedException();
+
+        public dynamic Selection => throw new NotImplementedException();
+
+        public bool Split { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public int SplitColumn { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public double SplitHorizontal { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public int SplitRow { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public double SplitVertical { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public double TabRatio { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+
+        public XlWindowType Type => throw new NotImplementedException();
+
+        public double UsableHeight => throw new NotImplementedException();
+
+        public double UsableWidth => throw new NotImplementedException();
+
+        public bool Visible { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+
+        public Range VisibleRange => throw new NotImplementedException();
+
+        public int WindowNumber => throw new NotImplementedException();
+
+        XlWindowState Window.WindowState { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public dynamic Zoom { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public XlWindowView View { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public bool DisplayRightToLeft { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+
+        public SheetViews SheetViews => throw new NotImplementedException();
+
+        public dynamic ActiveSheetView => throw new NotImplementedException();
+
+        public bool DisplayRuler { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public bool AutoFilterDateGrouping { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public bool DisplayWhitespace { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+
+        public int Hwnd => throw new NotImplementedException();
     }
-//}
+    
+    }
+
 
